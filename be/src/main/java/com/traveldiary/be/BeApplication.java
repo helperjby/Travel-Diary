3b118f6ad46d8f664aa98fb1c608dace46db5333
carpe-horas <144937088+carpe-horas@users.noreply.github.com>
2024-06-14 21:32:51 +0900

    Update BeApplication.java

--- be/src/main/java/com/traveldiary/be/BeApplication.java
+++ be/src/main/java/com/traveldiary/be/BeApplication.java
@@ -1,11 +1,21 @@
 package com.traveldiary.be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Objects;
 
 @SpringBootApplication
-public class BeApplication {
+public class BeApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DATASOURCE_URL", Objects.requireNonNull(dotenv.get("DATASOURCE_URL")));
+        System.setProperty("DATASOURCE_USERNAME", Objects.requireNonNull(dotenv.get("DATASOURCE_USERNAME")));
+        System.setProperty("DATASOURCE_PASSWORD", Objects.requireNonNull(dotenv.get("DATASOURCE_PASSWORD")));
+
         SpringApplication.run(BeApplication.class, args);
     }
 }
